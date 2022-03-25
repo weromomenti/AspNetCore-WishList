872424a8e5818697fe551599378388a062ad323d
weromomenti <tseretelig5@gmail.com>
2022-03-25 14:56:04 +0400

    First Module

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -17,7 +17,7 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,9 +27,10 @@ namespace WishList
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseExceptionHandler("/Home/Error");
-
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
