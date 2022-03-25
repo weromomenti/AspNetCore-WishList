def1aa6d7d9d67d98265b8d3aaf73cb135a7f1bc
weromomenti <tseretelig5@gmail.com>
2022-03-25 17:39:25 +0400

    Module 4

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
