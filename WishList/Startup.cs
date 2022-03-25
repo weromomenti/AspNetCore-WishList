d30a427d5fc378f4b4fe9fc44402ea039d65d60b
weromomenti <tseretelig5@gmail.com>
2022-03-25 14:54:26 +0400

    1.1

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -27,10 +27,9 @@ namespace WishList
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+
+            app.UseExceptionHandler("/Home/Error");
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
